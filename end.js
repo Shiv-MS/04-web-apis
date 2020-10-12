e58ad65c9640cebfec01a41ec9c1620c0a53d9c1
Shivam Shewa <shewashivam@gmail.com>
2020-10-12 01:58:34 -0500

    Redid App.  Seperated html files, JS files, and CSS files for better organization

---
+++ end.js
@@ -0,0 +1,34 @@
+const userName = document.querySelector('#winnerName');
+const saveScoreButton = document.querySelector('#saveScoreButton');
+const finalScore = document.querySelector('#finalScore');
+const mostRecentScore = document.querySelector('#mostRecentScore');
+
+const highScores = JSON.pasrse(localStorage.getItem('highScores')) || [];
+
+const Max_High_Scores = 5;
+
+finalScore.innerText = mostRecentScore
+
+userName.addEventListener('keyup', () => {
+    saveScoreButton.disabled = !userName.value
+})
+
+saveHighScore = e => {
+    e.preventDefault()
+
+    const score = {
+        score: mostRecentScore,
+        name: userName.value
+    }
+
+    highScores.push(score)
+
+    highScores.sort((a, b) => {
+        return b.score - a.score
+    })
+
+    highScores.splice(5)
+
+    localStorage.setItem('highScores', JSON.stringify(highScores))
+    window.location.assign('/')
+}
